1beceef356148c4295dc56f7ab793c16821748fc
BRIAN BETT <+>
2023-06-27 15:00:08 +0300

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
+  # Login
+  post '/login', to: "sessions#create"
+
+
+  #Stay logged in
+  get '/me', to: "users#show"
+
+  #Logout
+
+  delete "/logout", to: "sessions#destroy"
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
